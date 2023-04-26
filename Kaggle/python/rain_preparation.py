f6eea1dab968096d5e826f7e6dc5a21dece5186d
shahriar-arch <shahriartusher33@gmail.com>
2023-04-26 11:41:07 +0600

    casual update

--- Kaggle/python/rain_preparation.py
+++ Kaggle/python/rain_preparation.py
@@ -1,8 +1,12 @@
-# We need to import sys  to stope the code in the middle if the inputs are not right 
+# We need to import sys  to stope the code in the
+# middle if the inputs are not right
+"""Need to import sys to stop the code in the middle if someting not right"""
 import sys
+
 
 # write a function that will check if you are prepare for rain or not.
 def prepared_for_rain(have_umbrella, rain_level, have_hood, is_workday):
+    """A function that checks are you ready for a rainy day"""
     output = (
         have_umbrella
         or ((rain_level < 5 and have_hood)
@@ -12,45 +16,49 @@ def prepared_for_rain(have_umbrella, rain_level, have_hood, is_workday):
 
 
 # take the input from the user
-umbrella_status = (input("Have umbrella?(Ture or False): "))
-hood_status = (input("Have hood? (True or False) :"))
-workday_status = (input("Is it workday? (True or False): "))
+umbrella_status = input("Have umbrella?(Ture or False): ")
+hood_status = input("Have hood? (True or False) :")
+workday_status = input("Is it workday? (True or False): ")
 rain_status = int(input("Give the forcasted rain level (0 to 10): "))
+
 
 # convert the True or False values to bollean
 # write a function to convert the string True or False to bool True or False
-def get_bool(input):
-    output = None 
-    """A function that takes input string True or False and returns bool True or False"""
-    if input == "True":
+def get_bool(input_value):
+    """A function that takes input string True or False and returns bool True
+    or False"""
+    output = None
+    if input_value == "True":
         output = True
-    elif input == "False":
+    elif input_value == "False":
         output = False
     # if the input value is not true or false the function will return None
-    return output 
+    return output
 
-# convert the input values to bools 
+
+# convert the input values to bools
 umbrella_status = get_bool(umbrella_status)
 hood_status = get_bool(hood_status)
 workday_status = get_bool(workday_status)
 
-# we have to check if any of the value have none value 
-# to check we have to compare the variable with a none variable 
-# creating a none variable 
-x = None 
+# we have to check if any of the value have none value
+# to check we have to compare the variable with a none variable
+# creating a none variable
+X = None
 
 # now check the type of variables
 
-if type(x) == type(umbrella_status):
+if type(X) == type(umbrella_status):
     sys.exit("Invalid Input")
-if type(x) == type(hood_status):
+if type(X) == type(hood_status):
     sys.exit("Invalid Input")
-if type(x) == type(workday_status):
-    sys.exit("Invalid Input") 
+if type(X) == type(workday_status):
+    sys.exit("Invalid Input")
 
 
 # if everyting alright, run the given values in the function
-rain_preparation = prepared_for_rain(umbrella_status, rain_status, hood_status, workday_status)
+rain_preparation = prepared_for_rain(umbrella_status, rain_status, hood_status,
+                                     workday_status)
 
 # Display the result
 if rain_preparation:
